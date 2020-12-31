b5f71cc5a4b5bc8bbcdd4259e5391e7edfb59ce1
raunaksingh832 <60030141+raunaksingh832@users.noreply.github.com>
2020-12-31 11:23:15 +0530

    This can send a email by listening to you
    Hi! I am Raunak Singh a student of class 8 who is learning to code , here is one of my projects which can send emails by listening to you isnt that great ?

--- emailer.py
+++ emailer.py
@@ -1,6 +1,20 @@
 def sendemail():
     import smtplib
     import getpass
+    import speech_recognition as sr
+    listener = sr.Recognizer()
+    try:
+        with sr.Microphone() as source :
+            print('speak the subject.....')
+            voice = listener.listen(source)
+            sub = listener.recognize_google(voice)
+        with sr.Microphone() as source1:
+            print('speak the body of th mail....')
+            voice1 = listener.listen(source1)
+            body = listener.recognize_google(voice1)
+    except:
+        pass 
+        
     smtp_object = smtplib.SMTP('smtp.gmail.com',587)
     smtp_object.ehlo()
     smtp_object.starttls()
@@ -9,8 +23,9 @@ def sendemail():
     smtp_object.login(email,password)
     from_ = email
     to = input('to whom you want to send')
-    subject = input('subject:')
-    content = input('content :')
-    msg = 'Subject:' + subject + '\n' + content + '\n'+ "A EMAIL SENT USING RAUNAKS EMAILER APP!"
+    subject = sub
+    content = body
+    msg = 'Subject:' + subject + '\n' + content + '\n'+ "A EMAIL SENT USING voice RAUNAKS EMAILER APP!"
     smtp_object.sendmail(from_,to,msg)
     print ('email succesfuly sent !')
+sendemail()
